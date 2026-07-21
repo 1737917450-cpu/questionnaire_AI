411f4e5551d8c7441b9315becdb5669a7cc1c6d3
1737917450-cpu <1737917450@qq.com>
2026-07-21 17:30:59 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.QUESTIONNAIRE_CONFIG = {
+  submissionEndpoint: "https://script.google.com/macros/s/AKfycbwfYT12LszzMPQG3qaf1zZD8r-n-IzNsYWyIktP22MWXaTzLxPfHeZ271_jbjeXfEFaiQ/exec",
+  studyId: "ai_science_fund_proposal_evaluation_2026"
+};
