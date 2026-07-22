62227492d29c530b429adb75dec41ceba3a68c84
1737917450-cpu <1737917450@qq.com>
2026-07-22 16:05:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.QUESTIONNAIRE_CONFIG = {
-  submissionEndpoint: "https://script.google.com/macros/s/AKfycbzOb3SbhcZobpLO_NZUz2WgbFIUyxW-OVxBEWNMI2I-LNVuhuidG5SVzyKf7uSSjELv/exec",
+  submissionEndpoint: "https://script.google.com/macros/s/AKfycbwjK1GjD3oSNyQcAhWpBLJFJjofi7CeHTJQPwm7g4UFuvMbFnPiHigxq-i_hoos0Yo75g/exec",
   studyId: "ai_science_fund_proposal_evaluation_2026"
 };
